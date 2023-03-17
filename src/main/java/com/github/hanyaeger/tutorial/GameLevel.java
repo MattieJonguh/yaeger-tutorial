21821b61ef2fd7b68a7a95eb8e3e9bd672a53848
MattieJonguh <tim@woutersonline.eu>
2023-03-17 14:11:13 +0100

    health done

--- src/main/java/com/github/hanyaeger/tutorial/GameLevel.java
+++ src/main/java/com/github/hanyaeger/tutorial/GameLevel.java
@@ -3,7 +3,9 @@ package com.github.hanyaeger.tutorial;
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.scenes.DynamicScene;
 
-public class GameLevel extends DynamicScene {
+import java.util.Random;
+
+public class GameLevel extends DynamicScene{
     @Override
     public void setupScene() {
         setBackgroundAudio("audio/waterworld.mp3");
@@ -12,13 +14,15 @@ public class GameLevel extends DynamicScene {
 
     @Override
     public void setupEntities() {
+        //WIP
         var Swordfish = new Swordfish(
-                new Coordinate2D(getWidth() / 3, getHeight() / 3 * 2)
+                new Coordinate2D(getWidth() / 3,  new Random().nextInt((int) getHeight()- 81))
         );
+
         addEntity(Swordfish);
 
         var Hanny = new Hanny(
-                new Coordinate2D(50, 50)
+                new Coordinate2D(50, 50), new HealthText(new Coordinate2D(600, 100))
         );
         addEntity(Hanny);
     }
